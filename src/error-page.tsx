2b1d09b2dfcadfd2dc24619ee2cb2dc3b514217f
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-11-09 17:04:51 -0300

    refactor(ui): migrate error page component to TypeScript

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,16 +1,24 @@
 'use strict'
 
+import React from 'react'
 import { useRouteError } from 'react-router'
 
 export default function ErrorPage() {
   const error = useRouteError()
+
+  let errorMessage = 'Unknown error'
+  if (error instanceof Error) {
+    errorMessage = error.message
+  } else if (error && typeof error === 'object' && 'statusText' in error) {
+    errorMessage = (error as { statusText: string }).statusText || 'Unknown error'
+  }
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   )
