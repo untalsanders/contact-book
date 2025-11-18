bce5fce11e65ad232bd31c75c63aaf7943f69244
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-11-18 17:51:26 -0300

    refactor(ui): restructure contact list component and home page layout
    Move the "Contacts" header from the home page into the ContactList component
    to centralize the UI structure and remove duplication. Also remove background
    color from main layout for consistency.

--- src/components/contacts/ContactList.tsx
+++ src/components/contacts/ContactList.tsx
@@ -5,69 +5,74 @@ import { Form, NavLink } from 'react-router'
 import { Contact } from '../../types/contact.types'
 import ContactPhoto from './ContactPhoto'
 
-export default function ContactList({ contacts }: { contacts: Array<Contact> }) {
+export default function ContactList({ contacts }: { contacts: Contact[] }) {
   return (
-    <table className="table-auto w-full border-collapse border border-gray-400">
-      <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
-        <tr>
-          <th className="p-2 whitespace-nowrap border border-gray-300">
-            <div className="font-semibold text-left">NAME</div>
-          </th>
-          <th className="p-2 whitespace-nowrap border border-gray-300">
-            <div className="font-semibold text-left">PHONE NUMBER</div>
-          </th>
-          <th className="p-2 whitespace-nowrap border border-gray-300">
-            <div className="font-semibold text-left"></div>
-          </th>
-        </tr>
-      </thead>
-      <tbody className="text-sm divide-y divide-gray-100">
-        {contacts.map((contact: Contact) => (
-          <tr key={contact.id} className="hover:bg-gray-100">
-            <td className="p-2 whitespace-nowrap border border-gray-300">
-              <div className="flex items-center">
-                <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                  <ContactPhoto url='/images/avatar.jpeg' alt='' />
-                </div>
-                <div className="font-medium text-gray-800">
-                  {contact.firstname} {contact.lastname}
-                </div>
-              </div>
-            </td>
-            <td className="p-2 whitespace-nowrap border border-gray-300">
-              <div className="text-left">{contact.phone}</div>
-            </td>
-            <td className="p-2 whitespace-nowrap border border-gray-300">
-              <div className="flex items-center gap-2">
-                <NavLink
-                  to={`/contacts/${contact.id}`}
-                  className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
-                  Ver
-                </NavLink>
-                <NavLink
-                  to={`/contacts/${contact.id}/edit`}
-                  className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
-                  Editar
-                </NavLink>
-                <Form
-                  action={`/contacts/${contact.id}/destroy`}
-                  method="post"
-                  onSubmit={event => {
-                    if (!confirm('Por favor confirme que desea eliminar este contacto.')) {
-                      event.preventDefault()
-                    }
-                  }}>
-                  <button
-                    type="submit"
-                    className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
-                    Delete
-                  </button>
-                </Form>
-              </div>
-            </td>
+    <section className="grid grid-rows-[min-content_1fr] gap-y-4 px-4">
+      <h2 className="text-gray-500 text-[1.5rem] flex items-center gap-2">
+        Contacts <span className="text-[1rem]">({contacts.length})</span>
+      </h2>
+      <table className="table-auto w-full border-collapse border border-gray-400">
+        <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+          <tr>
+            <th className="p-2 whitespace-nowrap border border-gray-300">
+              <div className="font-semibold text-left">NAME</div>
+            </th>
+            <th className="p-2 whitespace-nowrap border border-gray-300">
+              <div className="font-semibold text-left">PHONE NUMBER</div>
+            </th>
+            <th className="p-2 whitespace-nowrap border border-gray-300">
+              <div className="font-semibold text-left"></div>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-sm divide-y divide-gray-100">
+          {contacts.map(contact => (
+            <tr key={contact.id} className="hover:bg-gray-100">
+              <td className="p-2 whitespace-nowrap border border-gray-300">
+                <div className="flex items-center">
+                  <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                    <ContactPhoto url="/images/avatar.jpeg" alt="" />
+                  </div>
+                  <div className="font-medium text-gray-800">
+                    {contact.firstname} {contact.lastname}
+                  </div>
+                </div>
+              </td>
+              <td className="p-2 whitespace-nowrap border border-gray-300">
+                <div className="text-left">{contact.phone}</div>
+              </td>
+              <td className="p-2 whitespace-nowrap border border-gray-300">
+                <div className="flex items-center gap-2">
+                  <NavLink
+                    to={`/contacts/${contact.id}`}
+                    className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
+                    Ver
+                  </NavLink>
+                  <NavLink
+                    to={`/contacts/${contact.id}/edit`}
+                    className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
+                    Editar
+                  </NavLink>
+                  <Form
+                    action={`/contacts/${contact.id}/destroy`}
+                    method="post"
+                    onSubmit={event => {
+                      if (!confirm('Por favor confirme que desea eliminar este contacto.')) {
+                        event.preventDefault()
+                      }
+                    }}>
+                    <button
+                      type="submit"
+                      className="cursor-pointer border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 px-4 py-2">
+                      Delete
+                    </button>
+                  </Form>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,14 +8,5 @@ import ContactListEmpty from '../components/contacts/ContactListEmpty'
 export default function HomePage() {
   const { contacts } = useLoaderData()
 
-  return (
-    <div className="grid grid-rows-[min-content_1fr] p-4">
-      <div>
-        <h2 className="text-gray-500 text-[1.5rem] flex items-center gap-2">
-          Contacts <span className="text-[1rem]">({contacts.length})</span>
-        </h2>
-      </div>
-      <div className="py-4">{contacts.length ? <ContactList contacts={contacts} /> : <ContactListEmpty />}</div>
-    </div>
-  )
+  return <div className="py-4">{contacts.length ? <ContactList contacts={contacts} /> : <ContactListEmpty />}</div>
 }
